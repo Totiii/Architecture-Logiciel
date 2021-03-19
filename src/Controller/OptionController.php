659f359efbff3badeef53356451ef8278afe4059
Warzcube <warzcube@gmail.com>
2021-03-19 15:47:02 +0100

    minor change

--- src/Controller/OptionController.php
+++ src/Controller/OptionController.php
@@ -8,15 +8,6 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class OptionController extends AbstractController
 {
-    /**
-     * @Route("/option", name="list_option", methods={"GET"})
-     */
-    public function list(): Response
-    {
-        return $this->render('option/list.html.twig', [
-            'controller_name' => 'OptionController',
-        ]);
-    }
 
     /**
      * @Route("/option/{id}", name="details_option", methods={"GET"})
